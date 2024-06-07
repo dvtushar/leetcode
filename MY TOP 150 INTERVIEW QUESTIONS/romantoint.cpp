0067809cdfe16d9e75e3d3ff49af7c798e0cbe7c
Tushar Dev <tushardev.work@gmail.com>
2024-06-07 16:04:51 +0530

    edited romantoint

--- MY TOP 150 INTERVIEW QUESTIONS/romantoint.cpp
+++ MY TOP 150 INTERVIEW QUESTIONS/romantoint.cpp
@@ -1,9 +1,31 @@
-#include <bitsstd++.h>
-using namespace std;
-class Solution
-{
+class Solution {
 public:
-    int romanToInt(string s)
-    {
+    int getvalue(char c){
+        switch(c){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            case 'L' : return 50;
+            default : return -1;
+
+        }
+    }
+    int romanToInt(string s) {
+        int res = 0;
+        int i = 0;
+        while(i<s.size()-1){
+            if(getvalue(s[i])>=getvalue(s[i+1])){
+                res += getvalue(s[i]);
+            }
+            else{
+                res -= getvalue(s[i]);
+            }
+            i++;
+        }
+        res +=getvalue(s[i]);
+        return res;
         }
 };
